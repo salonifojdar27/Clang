98c6379d398413c36e30e8d05b1482a030435641
salonifojdar27 <salonifojdar27@gmail.com>
2024-08-21 12:09:19 +0530

    Add files via upload

---
+++ pr4/Q2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int i,j,k=11;
+	
+	for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%d ",k);
+			k++;
+		}
+		printf("\n");
+	}
+
+}
---
+++ pr4/Q4.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int i,j,k;
+	
+	for(i=1;i<=5;i++)
+	{
+		for(k=1;k<=i;k++)
+		{
+		    printf(" ");
+		}
+		for(j=i;j<=5;j++)
+		{
+		  if(j%2==0)
+		  {
+		  	printf("0");
+		  }
+		  else
+		  {
+		  	printf("1");
+		  }
+		}
+		printf("\n");
+	}
+
+}
